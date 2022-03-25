89ac51f9ac327d0963a01b79da97918c71ede804
anhkhoatranle30 <anhkhoatranle30@gmail.com>
2022-03-25 10:02:40 +0700

    feat(user-model): is created by using mongoose ORM
    Keys are email, password, fullname, isVerified, isActive, isDeleted, secretOtp, googleId with timestamps. Override toJSON method to delete password, secretOtp, googleId. Created a method generateToken to generate jwt for that user. Add pre save hook to check if password is modified.

---
+++ src/api/v1/components/users/model.js
@@ -0,0 +1,82 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+const SALT_ROUNDS = 10;
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    fullname: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
+    secretOtp: {
+      type: String,
+    },
+    googleId: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.methods.toJSON = function () {
+  const userObject = this.toObject();
+  delete userObject.password;
+  delete userObject.secretOtp;
+  delete userObject.googleId;
+  return userObject;
+};
+
+userSchema.methods.generateToken = function () {
+  const user = this;
+  const token = jwt.sign(
+    {
+      data: {
+        id: user._id.toString(),
+        email: user.email,
+        isVerified: user.isVerified,
+      },
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: '1h' }
+  );
+  return token;
+};
+
+userSchema.pre('save', async function (next) {
+  const user = this;
+
+  if (user.isModified('password')) {
+    user.password = await bcrypt.hash(user.password, SALT_ROUNDS);
+  }
+  next();
+});
+
+const User = mongoose.model('User', userSchema);
+export default User;
