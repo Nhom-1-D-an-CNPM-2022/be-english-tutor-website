aedf63b3f9f59bf8e1089769a75b95b837dd6322
anhkhoatranle30 <anhkhoatranle30@gmail.com>
2022-03-26 21:12:06 +0700

    refactor(user-routes): remove /register/google route

--- src/api/v1/components/users/routes.js
+++ src/api/v1/components/users/routes.js
@@ -6,7 +6,6 @@ const userRoutes = express.Router();
 //======================== GET ========================
 //======================== POST ========================
 userRoutes.post('/register', userControllers.register);
-userRoutes.post('/register/google', userControllers.registerGoogle);
 //======================== PUT ========================
 //======================== DELETE ========================
 
