a5772368847bd5273fe39ad6895ae3806981fc68
truongvukt2000 <truongvukt2000@gmail.com>
2022-04-03 17:23:04 +0700

    change infor tutors getting

--- src/api/v1/components/tutor/controllers/getListTutors.js
+++ src/api/v1/components/tutor/controllers/getListTutors.js
@@ -1,11 +1,44 @@
+import User from "../../users/model";
 import Tutor from "../model";
 
+function getNumberOfDays(start, end) {
+  const date1 = new Date(start);
+  const date2 = new Date(end);
+  const oneDay = 1000 * 60 * 60 * 24;
+  const diffInTime = date2.getTime() - date1.getTime();
+  const diffInDays = Math.round(diffInTime / oneDay);
+  return diffInDays;
+}
+
 const getListTutors = (req, res) => {
-  Tutor.find({}, (err, tutors) => {
+  Tutor.find({}, async (err, tutors) => {
     if (err) {
       return res.status(500).json({ message: e });
     }
-    return res.status(200).json(tutors);
+
+    let results = tutors.map(async (tutor) => {
+      const user = await User.findOne({ _id: tutor._id });
+      if (user !== null) {
+        const today = new Date();
+        const currentDay =
+          today.getMonth() +
+          1 +
+          "/" +
+          today.getDate() +
+          "/" +
+          today.getFullYear();
+        const ageOfAccount = getNumberOfDays(user.createdAt, currentDay);
+        return {
+          fullname: user.fullname,
+          hometown: user.hometown,
+          introduce: tutor.introduce,
+          ageOfAccount: ageOfAccount,
+        };
+      }
+    });
+    Promise.all(results).then((result) => {
+      return res.status(200).json(result.filter((r) => r != null));
+    });
   });
 };
 
