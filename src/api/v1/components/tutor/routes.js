d7ae998eb3e41868ff128149b45b8e165ae55523
anhkhoatranle30 <anhkhoatranle30@gmail.com>
2022-04-03 22:16:43 +0700

    feat(v1-components-tutor-routes.js): add new route for searching tutors
    Add new route: '/search' to search for tutors.

--- src/api/v1/components/tutor/routes.js
+++ src/api/v1/components/tutor/routes.js
@@ -1,10 +1,11 @@
-import express from "express";
-import tutorController from "./controllers";
+import express from 'express';
+import tutorController from './controllers';
 
 const tutorRoutes = express.Router();
 
 //======================== GET ========================
-tutorRoutes.get("/", tutorController.getListTutors);
+tutorRoutes.get('/', tutorController.getListTutors);
+tutorRoutes.get('/search', tutorController.searchAllTutors);
 //======================== POST ========================
 //======================== PUT ========================
 //======================== DELETE ========================
