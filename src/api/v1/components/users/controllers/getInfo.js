51accaf675cf746aa586f7ca0c20b7cc01a02de9
SevenPain <nguyenhaanhkiem@gmail.com>
2022-04-13 00:30:42 +0700

    JWT Authentication

--- src/api/v1/components/users/controllers/getInfo.js
+++ src/api/v1/components/users/controllers/getInfo.js
@@ -1,17 +1,19 @@
-import User from '../model';
-import parseErrorIntoMessage from '../../../helpers/parseErrorIntoMessage';
+import User from "../model";
+import parseErrorIntoMessage from "../../../helpers/parseErrorIntoMessage";
 
 const getInfo = async (req, res) => {
   const { user } = req;
-  
+
   try {
+    console.log(res.locals.data);
+
     const userFindById = await User.findById(user._id);
-    if(userFindById === null) {
-        throw new Error('User is not found');
+    if (userFindById === null) {
+      throw new Error("User is not found");
     }
 
     res.status(200).send({
-        "user": userFindById
+      user: userFindById,
     });
   } catch (error) {
     res.status(400).send(parseErrorIntoMessage(error));
--- src/api/v1/middlewares/verifyToken.js
+++ src/api/v1/middlewares/verifyToken.js
@@ -1,21 +1,25 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
 const verifyToken = (req, res, next) => {
-    const authHeader = req.header('Authorization');
-    const accessToken = authHeader && authHeader.split(' ')[1];
+  // const authHeader = req.header("Authorization");
+  // const accessToken = authHeader && authHeader.split(" ")[1];
+  let token = req.query.jwt || req.body.jwt;
 
-    if(accessToken!==null) {
-        jwt.verify(accessToken, process.env.JWT_SECRET, (error, user) => {
-            if(error) {
-                res.status(403).send("Token is not valid");
-            }
-            req.user = user.data;
-            return next();
-        });
-    }
-    else {
-        res.status(401).send("You're not authenticated");
-    }
-}
+  if (token !== undefined) {
+    jwt.verify(token, process.env.JWT_SECRET, (error, user) => {
+      if (error) {
+        res.status(403).send("Token is not valid");
+
+        return;
+      }
+
+      req.user = user.data;
+
+      return next();
+    });
+  } else {
+    res.status(401).send("You're not authenticated");
+  }
+};
 
 export default verifyToken;
