01d7a4fddd4fa72b15365d161d57ecbcd33d809c
truongvukt2000 <truongvukt2000@gmail.com>
2022-04-30 13:44:23 +0700

    tutor create schedule

--- src/api/v1/components/schedule/controllers/scheduleReservationAvailability.js
+++ src/api/v1/components/schedule/controllers/scheduleReservationAvailability.js
@@ -1,24 +1,24 @@
-import Schedule from "../model";
 import parseErrorIntoMessage from "../../../helpers/parseErrorIntoMessage";
+import Schedule from "../model";
 
 const scheduleReservationAvailability = async (req, res) => {
-    const { user } = req;
-    //tutorId for test (Instead using user._id)
-    const { tutorId, scheduleTime} = req.body;
-    
-    try {
-        for(let time of scheduleTime) {
-            await new Schedule({
-                tutor: tutorId ,
-                startTime: time,
-                interval: 15,
-            }).save();
-        }
+  const { user } = req;
+  //tutorId for test (Instead using user._id)
+  const { tutorId, scheduleTime } = req.body;
 
-        res.status(200).send("Schedule reservation successfully");
-    } catch (error) {
-      res.status(400).send(parseErrorIntoMessage(error));
+  try {
+    for (let item of scheduleTime) {
+      await new Schedule({
+        tutor: tutorId,
+        startTime: item.time,
+        interval: item.interval,
+      }).save();
     }
-  };
-  
-  export default scheduleReservationAvailability;
+
+    res.status(200).send("Schedule reservation successfully");
+  } catch (error) {
+    res.status(400).send(parseErrorIntoMessage(error));
+  }
+};
+
+export default scheduleReservationAvailability;
