cdd216dabb7cec4af3373ae1f02c6bbcc45c5a8f
truongvukt2000 <truongvukt2000@gmail.com>
2022-05-01 20:23:39 +0700

    regiter tutor

--- src/api/v1/index.js
+++ src/api/v1/index.js
@@ -1,16 +1,20 @@
-import express from 'express';
-import connectMongoDB from './database/mongo';
-import startMiddleware from './start/middleware';
-import startRoutes from './start/routes';
-import dotenv from 'dotenv';
-import cookieParser from 'cookie-parser';
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import express from "express";
+import connectMongoDB from "./database/mongo";
+import startMiddleware from "./start/middleware";
+import startRoutes from "./start/routes";
 
 // Environment variables
-dotenv.config({ path: './src/api/v1/configs/.env' });
+dotenv.config({ path: "./src/api/v1/configs/.env" });
 
 // Init Variables
 const app = express();
 const port = process.env.PORT || 5000;
+
+// Limit size
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 //Cookie parser
 app.use(cookieParser());
