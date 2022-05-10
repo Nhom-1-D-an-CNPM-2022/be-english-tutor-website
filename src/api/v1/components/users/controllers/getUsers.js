285e66505151d16aa849fa85268bc0b7e9a5f65e
anhkhoatranle30 <anhkhoatranle30@gmail.com>
2022-05-10 10:45:37 +0700

    refactor(getusers controller) - use getAll() service

--- src/api/v1/components/users/controllers/getUsers.js
+++ src/api/v1/components/users/controllers/getUsers.js
@@ -1,13 +1,24 @@
-import User from "../model";
+import parseErrorIntoMessage from '../../../helpers/parseErrorIntoMessage';
+import userServices from '../services';
 
-const getUsers = (req, res) => {
-  User.find({}, (err, users) => {
-    if (err) {
-      console.log(err);
-      return res.status(400).json({ message: err });
-    }
+const getUsers = async (req, res) => {
+  try {
+    const users = await userServices.getAll();
+
     return res.status(200).send(users);
-  });
+  } catch (error) {
+    res.status(400).send(parseErrorIntoMessage(error));
+  }
 };
+
+// const getUsers = (req, res) => {
+//   User.find({}, (err, users) => {
+//     if (err) {
+//       console.log(err);
+//       return res.status(400).json({ message: err });
+//     }
+//     return res.status(200).send(users);
+//   });
+// };
 
 export default getUsers;
--- src/api/v1/components/users/services/index.js
+++ src/api/v1/components/users/services/index.js
@@ -1,11 +1,13 @@
 import getOneByEmail from './getOneByEmail';
 import getOne from './getOne';
+import getAll from './getAll';
 import createNewUser from './createNewUser';
 
 const userServices = {
   getOneByEmail,
   createNewUser,
   getOne,
+  getAll,
 };
 
 export default userServices;
