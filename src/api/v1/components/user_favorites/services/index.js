fca998b05232abe8faea473ce127f5861c324908
anhkhoatranle30 <anhkhoatranle30@gmail.com>
2022-05-20 20:30:22 +0700

    (feat) Create services to get a userFavorite doc in DB and to create a new one

---
+++ src/api/v1/components/user_favorites/services/index.js
@@ -0,0 +1,9 @@
+import getOneByUserId from './getOneByUserId';
+import create from './create';
+
+const userFavoriteServices = {
+  getOneByUserId,
+  create,
+};
+
+export default userFavoriteServices;
