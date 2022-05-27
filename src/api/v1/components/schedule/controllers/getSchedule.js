5d07446521a4a2fc7c9f8a13aed84c6f3767aa1f
SevenPain <nguyenhaanhkiem@gmail.com>
2022-05-27 23:22:11 +0700

    Fix router

--- src/api/v1/components/schedule/controllers/getSchedule.js
+++ src/api/v1/components/schedule/controllers/getSchedule.js
@@ -2,11 +2,11 @@ import ScheduleService from "../services";
 import parseErrorIntoMessage from "../../../helpers/parseErrorIntoMessage";
 
 const getSchedule = async (req, res) => {
-    try {
-        //get Condition by tutor or startTime
-        const condition = whereCondition(req);
+  try {
+    //get Condition by tutor or startTime
+    const condition = whereCondition(req);
 
-        const data = await ScheduleService.findByCondition(condition);
+    const data = await ScheduleService.findByCondition(condition);
 
     res.status(200).send(data);
   } catch (error) {
