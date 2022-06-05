31c014be852b798873883c594362c7d9a2f3d663
HienTran <49000443+hientranHT@users.noreply.github.com>
2022-06-05 19:24:17 +0700

    fix and add test case

--- test/tutor/tutor-searchAllTutor.test.js
+++ test/tutor/tutor-searchAllTutor.test.js
@@ -3,22 +3,58 @@ import chai, { expect } from 'chai';
 import searchAllTutors from "../../src/api/v1/components/tutor/services/searchAllTutors";
 const assert = chai.assert;
 describe('Feature Search All Tutor', ()=>{
-    it('search all tutor with properties null',  () => {
+    it('search all tutor with sortby null',  () => {
       const option={
         search: '',
         skip: '',
         limit: '',
         query: '',
-        sortBy: '',
+        sortBy: null,
         order: '',
       }
-      assert.throws(() =>  searchAllTutors(option), Error, "Timeout of 20000ms exceeded");
+      const result = searchAllTutors(option);
+      expect(result).to.not.have.property('sortBy');
   });
 
-  it('search all tutor with properties null',  () => {
+  it('search all tutor with null',  () => {
     const option= null;
     const result = searchAllTutors(option);
     expect(result).to.not.have.property('sortBy');
   });
-  
+
+  it('search all tutor',  () => {
+    const option={
+      search: '',
+      skip: '',
+      limit: '',
+      query: '',
+      sortBy: '',
+      order: '',
+    }
+    const result = searchAllTutors(option);
+  });
+
+  it('',  () => {
+    const option={
+      search: '',
+      skip: '',
+      limit: '',
+      query: '',
+      sortBy: '',
+      order: '',
+    }
+    const result = searchAllTutors(option);
+  });
+
+  it('',  () => {
+    const option={
+      search: '',
+      skip: '',
+      limit: '',
+      query: '',
+      sortBy: '',
+      order: '',
+    }
+    const result = searchAllTutors(option);
+  });
 })
